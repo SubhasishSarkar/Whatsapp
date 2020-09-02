48b0115dfeeb330d8c63f2b1fa238a6d4985128e
SubhasishSarkar <sarkarsubhasish207@gmail.com>
2020-09-02 11:30:27 +0000

    initial message

---
+++ src/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Sidebar() {
+    return (
+        <div className="sidebar">
+            <div className="sidebar__header">
+                header
+            </div>
+            <div className="sidebar__search">
+            srch
+            </div>
+            <div className="sidebar__chat">
+            chat
+            </div>
+        </div>
+    )
+}
+
+export default Sidebar
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyClM4HzVZbxUROBh3H52fUZNUWr4RmyCQU",
+  authDomain: "whatsapp-clone-258f9.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-258f9.firebaseio.com",
+  projectId: "whatsapp-clone-258f9",
+  storageBucket: "whatsapp-clone-258f9.appspot.com",
+  messagingSenderId: "61426072078",
+  appId: "1:61426072078:web:3336b3cd14de0bb4d45a5f",
+  measurementId: "G-7K2T2D1S30"
+};
