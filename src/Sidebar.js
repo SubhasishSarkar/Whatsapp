56e0780a45130b15c436b2a751e462624990cdba
SubhasishSarkar <sarkarsubhasish207@gmail.com>
2020-09-04 17:48:11 +0000

    sidebar chat

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -32,6 +32,9 @@ function Sidebar() {
             </div>
       </div>
             <div className="sidebar__chats">
+                <SidebarChat addNewChat/>
+                <SidebarChat/>
+                <SidebarChat/>
                 <SidebarChat/>
             </div>
         </div>
--- src/SidebarChat.js
+++ src/SidebarChat.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import { Avatar } from '@material-ui/core'
+import './SidebarChat.css'
+function SidebarChat({addNewChat}) {
 
-function SidebarChat() {
-    return (
-        <div>
-            hello
+    const createChat = () => {};
+
+    return !addNewChat ? (
+        <div className="sidebarChat">
+            <Avatar src={`https://avatars.dicebear.com/api/human/abc.svg`}/>
+            <div className="sidebarChat__info">
+                <h2>Room Name</h2>
+                <p>Last text ....</p>
+            </div>
         </div>
-    )
+    ) : (
+        <div className="sidebarChat" onclick={createChat}>
+            <h2>Add New Chat</h2>
+        </div>
+    );
 }
 
 export default SidebarChat
